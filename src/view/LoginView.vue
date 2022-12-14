865ab3948ce0cd68cb01854b0676e44808028854
bekzod <bekzodbaratov334@gmail.com>
2022-12-15 00:14:13 +0500

    sdsd

---
+++ src/view/LoginView.vue
@@ -0,0 +1,1 @@
+<template>salommm</template>
---
+++ src/view/ServicesView.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="bg-white c-black">
+    <div class="mx-auto max-w-2xl py-16 px-4 sm:py-14 sm:px-6 lg:max-w-7xl lg:px-8">
+      <div class="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 xl:gap-x-8">
+        <RouterLink v-for="product in store.products" :key="product.id" :to="'/services/' + product.id" class="group">
+          <div
+            class="relative max-w-sm transition-all duration-300 cursor-pointer filter grayscale group-hover:grayscale-0"
+          >
+            <img
+              src="../assets/img/ServicesImages/img.png"
+              alt="{product.imageAlt}"
+              class="h-full w-full object-cover object-center"
+            />
+            <div class="absolute bottom-7 text-left mx-10 text-white">
+              <h2 class="text-xl font-medium">{{ product.name }}</h2>
+              <h3 class="hidden transition-all duration-300 group-hover:block">{{ product.price }}</h3>
+            </div>
+          </div>
+        </RouterLink>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { RouterLink } from "vue-router";
+import { useServices } from "../store/services";
+const store = useServices();
+</script>
