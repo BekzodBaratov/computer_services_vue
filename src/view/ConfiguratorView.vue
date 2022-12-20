78f7a3c0876d159e183645150dc5e5b692c7b6db
bekzod <bekzodbaratov334@gmail.com>
2022-12-20 15:19:21 +0500

    configurator respons end

--- src/view/ConfiguratorView.vue
+++ src/view/ConfiguratorView.vue
@@ -1,94 +1,120 @@
 <template>
   <div class="container mx-auto">
-    <div class="header grid grid-cols-2 justify-between items-center pt-[44px] ">
+    <div class="header grid grid-cols-1 md:grid-cols-2 gap-6 justify-between items-center pt-10">
       <div class="">
-        <h2 class="text-[#002E69] text-[56px] leading-[80.92px] font-semibold">КОНФИГУРАТОР ПК</h2>
-        <p class="text-[32px] font-normal leading-[46.24px] text-[#002E69] mt-[21px]">Сборка компьютера, о котором вы
-          мечтали.
-          Вы можете изменить комплектацию представ-ленных на сайте сборок, узнать цену онлайн, сравнить характеристики.
-          Оформите заказ и получите готовый Кейс.</p>
+        <h2 class="text-[#002E69] text-5xl font-semibold">КОНФИГУРАТОР ПК</h2>
+        <p class="text-3xl font-normal text-[#002E69] mt-8">
+          Сборка компьютера, о котором вы мечтали. Вы можете изменить комплектацию представ-ленных на сайте сборок,
+          узнать цену онлайн, сравнить характеристики. Оформите заказ и получите готовый Кейс.
+        </p>
       </div>
-      <div class="w-full">
-        <img src="src/assets/img/static/Group.svg" class="w-full h-[569px]">
+      <div class="hidden md:block mx-auto">
+        <img src="src/assets/img/static/Group.svg" class="h-[36rem] float-right" />
       </div>
     </div>
     <!-- pastki section -->
-    <div class="body grid grid-cols-[4fr,1fr] mt-11 gap-6 mb-[90px]">
-      <div class="grid grid-cols-[2fr,4fr] bg-[#4f87d30d]">
-        <div class="px-[20px] py-[10px]">
-          <div class="mt-[20px]" v-for="item, index in 10" :key="index">
-            <p
-              class="font-medium text-[32px] leading-[46px] text-[#002e6980] cursor-pointer transition-all duration-300 hover:text-[#002E69]">
-              Процессор</p>
+    <div class="body grid grid-cols-1 md:grid-cols-[4fr,1fr] mt-11 gap-6 mb-20">
+      <div class="block md:hidden mx-auto bg-[#4f87d30d] py-12 w-full">
+        <div class="flex flex-row md:flex-col">
+          <div class="mx-auto">
+            <img class="w-40" src="src/assets/img/static/comp.svg" />
+          </div>
+          <div class="mx-auto mt-12">
+            <div class="bg-[#AECBF4] rounded-xl px-4 py-2 w-56">
+              <p class="text-2xl text-[#D52C55] font-semibold text-center pb-2">790 000 сум</p>
+              <div class="flex gap-1">
+                <ButtonFill>Купить</ButtonFill>
+                <div
+                  @click="savedFunc"
+                  class="p-2 rounded-lg border border-[#002E69] flex items-center justify-center cursor-pointer"
+                >
+                  <div class="shopcart flex flex-col items-center">
+                    <i class="fa-heart text-[2rem] text-[#002E69]" :class="isSaved ? 'fa-regular' : 'fa-solid'"></i>
+                  </div>
+                </div>
+                <span class="p-2 rounded-lg border border-[#002E69] flex items-center justify-center cursor-pointer">
+                  <img src="../assets/img/magazin/shopping-cart.svg" alt="" />
+                </span>
+              </div>
+            </div>
           </div>
         </div>
-        <div class="pl-[30px] pr-[50px]  h-[100vh] overflow-y-scroll my-[60px]">
+      </div>
+
+      <div class="grid divide-y-reverse grid-cols-1 md:grid-cols-[2fr,4fr] bg-[#4f87d30d]">
+        <div class="hidden md:block px-5 py-2">
+          <div class="mt-2 text-base" v-for="(item, index) in 10" :key="index">
+            <p
+              class="font-medium leading-[2rem] text-[#002e6980] cursor-pointer transition-all duration-300 hover:text-[#002E69]"
+            >
+              Процессор
+            </p>
+          </div>
+        </div>
+        <div class="pl-8 pr-12 md:max-h-[100vh] overflow-y-scroll my-14">
           <!-- select option 1 -->
-          <div class="flex justify-between gap-[15px] items-center mb-[80px]" v-for="item, index in 2" :key="index">
-            <p class="text-[#002E69] text-[32px] leading-[46px] font-medium">Материнская плата</p>
-            <div class="size w-[230px] h-[62px]">
-
-              <select id="countries"
-                class="bg-transparent border border-[#002E69] text-[#002e694d] text-[22px] leading-[46px] rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+          <div class="flex justify-between gap-4 items-center mb-8" v-for="(item, index) in 2" :key="index">
+            <p class="text-[#002E69] text-base leading-[3rem] font-medium">Материнская плата</p>
+            <div class="size w-32">
+              <select
+                id="countries"
+                class="bg-transparent border border-[#002E69] text-[#002e694d] leading-[3rem] rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full px-2.5 py-1 outline-none"
+              >
                 <option selected>Size By</option>
-                <option v-for="(item, index) in 6" :key="index">
-                  option
-                </option>
-
+                <option v-for="(item, index) in 6" :key="index">option</option>
               </select>
             </div>
           </div>
           <!-- input tupy radio -->
-          <div class="mb-[40px] mx-auto">
-            <div class="" v-for="item, index in 3" :key="index">
-              <div class="flex gap-[20px] items-center mt-2">
-                <input type="radio" id="index" name="index" class="">
+          <div class="mb-10 mx-auto">
+            <div class="" v-for="(item, index) in 3" :key="index">
+              <div class="flex gap-5 items-center mt-2">
+                <input type="radio" :id="index" name="index" />
                 <div>
-                  <p class="text-[#002E69] text-[24px] leading-[35px] font-medium">Intel Box</p>
-                  <p class="text-[#D52C55] text-[16px] leading-[23px] font-normal">80 000 сум</p>
+                  <p class="text-[#002E69] text-base leading-[35px] font-medium">Intel Box</p>
+                  <p class="text-[#D52C55] text-base leading-[23px] font-normal">80 000 сум</p>
                 </div>
               </div>
             </div>
           </div>
           <!-- select option 2 -->
-          <div class="flex justify-between gap-[15px] items-center mb-[80px]" v-for="item, index in 8" :key="index">
-            <p class="text-[#002E69] text-[32px] leading-[46px] font-medium">Материнская плата</p>
-            <div class="size w-[230px] h-[62px]">
-
-              <select id="countries"
-                class="bg-transparent border border-[#002E69] text-[#002e694d] text-[22px] leading-[46px] rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+          <div class="flex justify-between gap-4 items-center mb-12" v-for="(item, index) in 8" :key="index">
+            <p class="text-[#002E69] text-base leading-[3rem] font-medium">Материнская плата</p>
+            <div class="size w-32">
+              <select
+                id="countries"
+                class="bg-transparent border border-[#002E69] text-[#002e694d] leading-[3rem] rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full px-2.5 py-1 outline-none"
+              >
                 <option selected>Size By</option>
-                <option v-for="(item, index) in 6" :key="index">
-                  option {{ index + 1 }}
-                </option>
-
+                <option v-for="(item, index) in 6" :key="index">option</option>
               </select>
             </div>
           </div>
         </div>
       </div>
       <!-- left result -->
-      <div class="mx-auto bg-[#4f87d30d] py-[50px] px-[68px]">
-        <div class="flex flex-col">
+      <div class="hidden md:block mx-auto bg-[#4f87d30d] py-12 w-full">
+        <div class="flex flex-row md:flex-col">
           <div class="mx-auto">
-            <img src="src/assets/img/static/comp.svg">
+            <img class="w-40" src="src/assets/img/static/comp.svg" />
           </div>
-          <div class="bg-[#aecbf433] rounded-[10px] px-[23px] pb-[22px] pt-2 text-center w-[257px] mt-[32px]">
-            <p class="text-[#D52C55] font-bold text-[32px] leading-[46px]">100 000 сум</p>
-            <div class="flex gap-2 mt-2 ">
-              <div
-                class="bg-[#002E69] transition-all duration-500 hover:bg-black rounded-[15px] px-4 py-2 text-[#F4F6F9] text-[24px] cursor-pointer leading-[35px] font-normal flex justify-center items-center">
-                Купить
+          <div class="mx-auto mt-12">
+            <div class="bg-[#AECBF4] rounded-xl px-4 py-2 w-56">
+              <p class="text-2xl text-[#D52C55] font-semibold text-center pb-2">790 000 сум</p>
+              <div class="flex gap-1">
+                <ButtonFill>Купить</ButtonFill>
+                <div
+                  @click="savedFunc"
+                  class="p-2 rounded-lg border border-[#002E69] flex items-center justify-center cursor-pointer"
+                >
+                  <div class="shopcart flex flex-col items-center">
+                    <i class="fa-heart text-[2rem] text-[#002E69]" :class="isSaved ? 'fa-regular' : 'fa-solid'"></i>
+                  </div>
+                </div>
+                <span class="p-2 rounded-lg border border-[#002E69] flex items-center justify-center cursor-pointer">
+                  <img src="../assets/img/magazin/shopping-cart.svg" alt="" />
+                </span>
               </div>
-              <div
-                class="border border-solid border-[#002E69] rounded-[15px] flex justify-center items-center px-[9px] py-[11px] cursor-pointer w-[50px] h-[50px]">
-                <i class="fa-regular fa-heart text-[30px]"></i>
-              </div>
-              <div
-                class="border border-solid border-[#002E69] rounded-[15px] flex justify-center items-center px-[9px] py-[11px] cursor-pointer w-[50px] h-[50px]">
-                <img src="src/assets/img/static/cart.svg" class="">
-              </div>
-
             </div>
           </div>
         </div>
@@ -98,10 +124,7 @@
 </template>
 
 <script setup>
-
-
-
-
+import ButtonFill from "../components/buttons/ButtonFill.vue";
 </script>
 
 <style scoped>
